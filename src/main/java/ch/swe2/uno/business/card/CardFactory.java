0f47f9feba65764ce7bde3848a5ce0ca68bae3fe
booooza <booooza@gmail.com>
2019-03-17 00:29:04 +0100

    Implement a card factory

---
+++ src/main/java/ch/swe2/uno/business/card/CardFactory.java
@@ -0,0 +1,38 @@
+package ch.swe2.uno.business.card;
+
+import java.awt.*;
+
+/**
+ * Class used to create cards
+ * Implementation of a singleton factory
+ */
+public class CardFactory {
+    /**
+     * Defines the private instance attribute
+     */
+    private static CardFactory ourInstance = new CardFactory();
+
+    /**
+     * Constructor must be private for singleton
+     */
+    private CardFactory() {}
+
+    /**
+     * Return the singleton instance
+     * @return ourInstance
+     */
+    public static CardFactory getInstance() {
+        return ourInstance;
+    }
+
+    /**
+     * Create a single card
+     * @return ICard card
+     */
+    public ICard createCard(Color color, int number) {
+        ICard card = new NumberCard(color, number);
+        return card;
+    }
+
+
+}
