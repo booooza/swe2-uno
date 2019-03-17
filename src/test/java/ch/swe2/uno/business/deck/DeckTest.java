baadd0dc405367608de16233cca332bc72bb2430
booooza <booooza@gmail.com>
2019-03-17 13:49:57 +0100

    Add first unit tests for deck and player

--- src/test/java/ch/swe2/uno/business/deck/DeckTest.java
+++ src/test/java/ch/swe2/uno/business/deck/DeckTest.java
@@ -3,11 +3,17 @@ package ch.swe2.uno.business.deck;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
-@DisplayName("DeckTest")
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("Deck")
 public class DeckTest {
     @Test
-    @DisplayName("Test something")
-    public void testSomething() {
-
+    @DisplayName("Number of cards")
+    public void testIfDeckContainsCorrectNumberOfCards() {
+        Deck deck = Deck.getInstance();
+        deck.create();
+        int expected = 76;
+        int actual = deck.getDeckSize();
+        assertEquals(expected, actual);
     }
 }
--- src/test/java/ch/swe2/uno/business/player/PlayerTest.java
+++ src/test/java/ch/swe2/uno/business/player/PlayerTest.java
@@ -3,11 +3,17 @@ package ch.swe2.uno.business.player;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
-@DisplayName("PlayerTest")
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("Player")
 public class PlayerTest {
     @Test
-    @DisplayName("Test something")
-    public void testSomething() {
-
+    @DisplayName("Name")
+    public void testIfPlayerGetsCorrectName() {
+        PlayerFactory playerFactory = PlayerFactory.getInstance();
+        IPlayer player = playerFactory.createPlayer("Player");
+        String expected = "Player";
+        String actual = player.getName();
+        assertEquals(expected, actual);
     }
 }
