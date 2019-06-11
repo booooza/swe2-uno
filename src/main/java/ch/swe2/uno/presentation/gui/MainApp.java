20698fe9c4302d33e768a11f43eab37f16889ad8
Luca Hostettler <hostettler.luca1896@gmail.com>
2019-06-11 20:13:49 +0200

    Update Views

--- src/main/java/ch/swe2/uno/presentation/gui/MainApp.java
+++ src/main/java/ch/swe2/uno/presentation/gui/MainApp.java
@@ -1,7 +1,6 @@
 package ch.swe2.uno.presentation.gui;
 
 import ch.swe2.uno.presentation.gui.controller.MainController;
-
 import com.jfoenix.assets.JFoenixResources;
 import com.jfoenix.controls.JFXDecorator;
 import com.jfoenix.svg.SVGGlyph;
@@ -11,10 +10,8 @@ import io.datafx.controller.flow.context.FXMLViewFlowContext;
 import io.datafx.controller.flow.context.ViewFlowContext;
 import javafx.application.Application;
 import javafx.collections.ObservableList;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,6 +30,14 @@ public class MainApp extends Application {
 		logger.info("Main ended");
 	}
 
+	public static Stage getPrimaryStage() {
+		return primaryStage;
+	}
+
+	private void setPrimaryStage(Stage pStage) {
+		MainApp.primaryStage = pStage;
+	}
+
 	@Override
 	public void start(Stage stage) throws Exception {
 		Flow flow = new Flow(MainController.class);
@@ -45,14 +50,14 @@ public class MainApp extends Application {
 		decorator.setCustomMaximize(true);
 		decorator.setGraphic(new SVGGlyph(""));
 
-		double width = 800;
-		double height = 600;
-		try {
-			Rectangle2D bounds = Screen.getScreens().get(0).getBounds();
-			width = bounds.getWidth() / 2.5;
-			height = bounds.getHeight() / 1.35;
-		} catch (Exception e) {
-		}
+		double width = 600;
+		double height = 400;
+		//try {
+		//	Rectangle2D bounds = Screen.getScreens().get(0).getBounds();
+		//	width = bounds.getWidth() / 2.5;
+		//	height = bounds.getHeight() / 1.35;
+		//} catch (Exception e) {
+		//}
 
 		Scene scene = new Scene(decorator, width, height);
 		final ObservableList<String> stylesheets = scene.getStylesheets();
@@ -66,13 +71,5 @@ public class MainApp extends Application {
 		stage.getIcons().add(new Image(MainApp.class.getResourceAsStream("/images/logo.png")));
 		stage.setScene(scene);
 		stage.show();
-	}
-
-	public static Stage getPrimaryStage() {
-		return primaryStage;
-	}
-
-	private void setPrimaryStage(Stage pStage) {
-		MainApp.primaryStage = pStage;
 	}
 }
