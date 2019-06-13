192abbb492aa1b0a6fe34cfa14753f47ffc651e3
Luca Hostettler <hostettler.luca1896@gmail.com>
2019-06-13 15:37:36 +0200

    Update MainApp Test

--- src/test/java/ch/swe2/uno/presentation/gui/MainAppTest.java
+++ src/test/java/ch/swe2/uno/presentation/gui/MainAppTest.java
@@ -1,8 +1,16 @@
 package ch.swe2.uno.presentation.gui;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import ch.swe2.uno.presentation.gui.controller.MainController;
+import com.jfoenix.assets.JFoenixResources;
+import com.jfoenix.controls.JFXDecorator;
+import com.jfoenix.svg.SVGGlyph;
+import io.datafx.controller.flow.Flow;
+import io.datafx.controller.flow.container.DefaultFlowContainer;
+import io.datafx.controller.flow.context.FXMLViewFlowContext;
+import io.datafx.controller.flow.context.ViewFlowContext;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
 import javafx.stage.Stage;
@@ -14,12 +22,36 @@ import org.testfx.framework.junit.ApplicationTest;
 
 public class MainAppTest extends ApplicationTest {
 
+	@FXMLViewFlowContext
+	private ViewFlowContext flowContext;
+
+
 	@Override
 	public void start(Stage stage) throws Exception {
-		Parent mainNode = FXMLLoader.load(MainApp.class.getClassLoader().getResource("fxml/main.fxml"));
-		stage.setScene(new Scene(mainNode));
+		Flow flow = new Flow(MainController.class);
+		DefaultFlowContainer container = new DefaultFlowContainer();
+		flowContext = new ViewFlowContext();
+		flowContext.register("Stage", stage);
+		flow.createHandler(flowContext).start(container);
+
+		JFXDecorator decorator = new JFXDecorator(stage, container.getView());
+		decorator.setCustomMaximize(true);
+		decorator.setGraphic(new SVGGlyph(""));
+
+		double height = 625.0;
+		double width = 1000.0;
+
+		Scene scene = new Scene(decorator, width, height);
+		final ObservableList<String> stylesheets = scene.getStylesheets();
+		stylesheets.addAll(JFoenixResources.load("css/jfoenix-fonts.css").toExternalForm(),
+				JFoenixResources.load("css/jfoenix-design.css").toExternalForm(),
+				MainApp.class.getResource("/css/uno-dark.css").toExternalForm());
+
+		stage.setTitle("UNO Game");
+		stage.getIcons().add(new Image(MainApp.class.getResourceAsStream("/images/logo.png")));
+		stage.setScene(scene);
 		stage.show();
-		stage.toFront();
+		//
 	}
 
 	@Before
